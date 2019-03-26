78681239df26218c212d0de7a9a7e1724384211f
Feyyaz Yılmaz <fyyz.ylmz.bjk@gmail.com>
2019-03-26 22:44:01 +0200

    php switch case

---
+++ swithc case php.php
@@ -0,0 +1,23 @@
+<?php
+
+$gun = 8;
+	   switch ($gun) {
+		   case 1; 
+		       echo "Pazartesi"; break;
+		   case 2; 
+		       echo "Sali"; break;
+		   case 3; 
+		       echo "Carsamba"; break;
+		   case 4; 
+		       echo "Persembe"; break;
+		   case 5; 
+		       echo "Cuma"; break;
+		   case 6; 
+		       echo "Cumartesi"; break;
+		   case 7; 
+		       echo "Pazar"; break;
+		  default :
+	   echo "Gun degeri 1-7 arasinda olmalidir"; }
+
+
+?>
